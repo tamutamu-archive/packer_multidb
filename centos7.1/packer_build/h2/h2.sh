c8a9ce109cff80e88353b33cc3e92abdf6b261ec
Your Name <you@example.com>
2016-02-02 14:39:59 +0900

    modify h2.

--- centos7.1/packer_build/h2/h2.sh
+++ centos7.1/packer_build/h2/h2.sh
@@ -3,6 +3,9 @@ CURDIR=$(cd $(dirname $0); pwd)
 wget http://www.h2database.com/h2-2016-01-21.zip -O /tmp/h2.zip
 unzip /tmp/h2.zip -d /var/lib/
 
+mkdir -p /var/run/h2
+mkdir -p /var/lib/h2/db
+
 cp $CURDIR/conf/initd/h2 /etc/init.d/
 chmod a+x /etc/init.d/h2
-##sudo systemctl enable h2
+sudo systemctl enable h2
